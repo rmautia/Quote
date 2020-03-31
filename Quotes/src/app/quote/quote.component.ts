09732b40007d221882b97b84da98d65b572a03f4
rmautia <raphaelnyangenya@gmail.com>
2020-03-31 21:57:45 +0300

    add highlight directive.

--- Quotes/src/app/quote/quote.component.ts
+++ Quotes/src/app/quote/quote.component.ts
@@ -20,6 +20,11 @@ export class QuoteComponent implements OnInit {
   toggleDetails(index){
     this.quotes[index].showDescription = !this.quotes[index].showDescription;
   }
+  completeGoal(isComplete, index){
+    if (isComplete) {
+      this.quotes.splice(index, 1);
+    }
+  }
 
   constructor() { }
 
